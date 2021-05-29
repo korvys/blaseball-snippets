524caf6e22a993183f228d8910704f7c6dc7edb7
korvys <thesilentprophet+github@gmail.com>
2021-05-30 01:04:59 +1000

    Minor reformat and added comments

--- strength_of_schedule.sql
+++ strength_of_schedule.sql
@@ -1,8 +1,10 @@
 -- Strength of Schedule, using SOS = (2(OR)+(OOR))/3
+-- SoS average is 0.5, higher is harder schedule.
 with season_games as (
 	select *
 	from data.games g
-	where g.season = 16 and g.day < 45
+	where g.season = (select max(tm.season) from data.time_map tm)
+	and g.day < least((select max(tm.day) from data.time_map tm where tm.season=g.season) , 99)
 ),
 record as (
 	select 	t.team_id, 
@@ -35,18 +37,21 @@ opp_opp_record as (
 	join opp_record opp on (opp.team_id = g.home_team or opp.team_id = g.away_team) and r.team_id != opp.team_id
 	group by 	r.team_id,
 				r.nickname
+),
+sos as (
+	select 	r.team_id,
+			r.nickname,
+			r.wins,
+			r.loses,
+			o_r.wins as opp_wins,
+			o_r.loses as opp_loses,
+			oor.wins as opp_opp_wins,
+			oor.loses as opp_opp_loses,
+			trunc((2*(o_r.wins::decimal/(o_r.wins+o_r.loses))+(oor.wins::decimal/(oor.wins+oor.loses)))/3, 4) as sos
+	from record r
+	join opp_record o_r on r.team_id = o_r.team_id
+	join opp_opp_record oor on r.team_id = oor.team_id
+	order by sos desc
 )
-select 	r.team_id,
-		r.nickname,
-		r.wins,
-		r.loses,
-		o_r.wins as opp_wins,
-		o_r.loses as opp_loses,
-		oor.wins as opp_opp_wins,
-		oor.loses as opp_opp_loses,
-		trunc((2*(o_r.wins::decimal/(o_r.wins+o_r.loses))+(oor.wins::decimal/(oor.wins+oor.loses)))/3, 4) as sos
-from record r
-join opp_record o_r on r.team_id = o_r.team_id
-join opp_opp_record oor on r.team_id = oor.team_id
-order by sos desc
-
+select avg(sos)
+from sos
